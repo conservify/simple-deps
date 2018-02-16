cba0ad78cd3f22381e62a50f1ff53325bfd849b6
Jacob Lewallen <jlewallen@gmail.com>
2018-02-16 15:00:22 -0800

    Support for pinning specific versions.

--- simple-deps.go
+++ simple-deps.go
@@ -3,20 +3,44 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 	"log"
 	"net/url"
 	"os"
 	"path"
+	_ "regexp"
 	"strings"
 )
 
 type Library struct {
-	Name string
+	Name    string
+	Version string
 }
 
 type Dependencies struct {
 	Libraries []*Library
+}
+
+func (d *Dependencies) Write(path string) error {
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return nil
+	}
+
+	defer f.Close()
+
+	for _, lib := range d.Libraries {
+		if lib.Version != "" {
+			f.WriteString(fmt.Sprintf("%s %s\n", lib.Name, lib.Version))
+		} else {
+			f.WriteString(fmt.Sprintf("%s\n", lib.Name))
+		}
+	}
+
+	return nil
+
 }
 
 func readDependencies(path string) (*Dependencies, error) {
@@ -24,14 +48,22 @@ func readDependencies(path string) (*Dependencies, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	defer file.Close()
 
 	var libs []*Library
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
+		fields := strings.Split(line, " ")
+		nameOrPath := fields[0]
+		version := ""
+		if len(fields) > 1 {
+			version = fields[1]
+		}
 		libs = append(libs, &Library{
-			Name: line,
+			Name:    nameOrPath,
+			Version: version,
 		})
 	}
 	deps := &Dependencies{
@@ -43,12 +75,30 @@ func readDependencies(path string) (*Dependencies, error) {
 type configuration struct {
 	Configuration string
 	Directory     string
+	UseLatest     bool
+}
+
+func GetRepositoryHash(path string) (h plumbing.Hash, err error) {
+	r, err := git.PlainOpen(path)
+	if err != nil {
+		return plumbing.ZeroHash, err
+	}
+
+	ref, err := r.Head()
+	if err != nil {
+		return plumbing.ZeroHash, err
+	}
+
+	h = ref.Hash()
+
+	return
 }
 
 func main() {
 	config := configuration{}
 	flag.StringVar(&config.Configuration, "config", "arduino-libraries", "libraries file")
 	flag.StringVar(&config.Directory, "dir", "./gitdeps", "where to cache libraries")
+	flag.BoolVar(&config.UseLatest, "use-latest", false, "use the latest version of libraries")
 
 	flag.Parse()
 
@@ -56,6 +106,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 	}
+
+	modified := false
 
 	for _, lib := range deps.Libraries {
 		if url, err := url.ParseRequestURI(lib.Name); err == nil {
@@ -89,11 +141,57 @@ func main() {
 				if err != nil && err != git.NoErrAlreadyUpToDate {
 					log.Fatalf("Error: %v", err)
 				}
+
+				w, err := r.Worktree()
+				if err != nil {
+					log.Fatalf("Error: %v", err)
+				}
+
+				if config.UseLatest {
+					err = r.Pull(&git.PullOptions{
+						RemoteName: "origin",
+					})
+					if err != nil && err != git.NoErrAlreadyUpToDate {
+						log.Fatalf("Error: %v", err)
+					}
+				} else {
+					if lib.Version != "" {
+						log.Printf("Checkout out %s", lib.Version)
+						err = w.Checkout(&git.CheckoutOptions{
+							Hash:  plumbing.NewHash(lib.Version),
+							Force: true,
+						})
+						if err != nil {
+							log.Fatalf("Error: %v", err)
+						}
+					}
+				}
+
+				ref, err := r.Head()
+				if err != nil {
+					log.Fatalf("Error: %v", err)
+				}
+
+				newVersion := ref.Hash().String()
+				if lib.Version != newVersion {
+					lib.Version = newVersion
+					modified = true
+				}
 			}
 		} else {
 			if s, err := os.Stat(lib.Name); err == nil && s.IsDir() {
-				log.Printf("Using directory %v", lib.Name)
+				version, err := GetRepositoryHash(lib.Name)
+				if err != nil {
+					log.Fatalf("Error: %v", err)
+				}
+
+				log.Printf("Using directory %v (%v)", lib.Name, version)
 			}
 		}
+	}
+
+	if modified {
+		log.Printf("Wrote new configuration")
+		deps.Write("arduino-libraries")
 	}
 }
