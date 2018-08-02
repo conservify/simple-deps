6e7721526c1ddbb2484c8a5f42bab07e4d6d35a3
Jacob Lewallen <jlewallen@gmail.com>
2018-08-01 19:54:24 -0700

    Disable shallow clone, didn't think this through.

--- repos.go
+++ repos.go
@@ -44,7 +44,6 @@ func CloneDependency(lib *Library, directory string, useHead bool) (clonePath st
 
 		_, err := git.PlainClone(p, false, &git.CloneOptions{
 			URL:      lib.UrlOrPath,
-			Depth:    1,
 			Progress: os.Stdout,
 		})
 		if err != nil {
